749f7c8cad87e52cc27e3995d588927b29b3fb82
radhajain <jainr@stanford.edu>
2018-07-10 16:39:22 -0700

    Trying new key !

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -15,7 +15,6 @@ const GridBlock = CompLibrary.GridBlock;
 const siteConfig = require(process.cwd() + '/siteConfig.js');
 
 
-
 function imgUrl(img) {
   return siteConfig.baseUrl + 'img/' + img;
 }
